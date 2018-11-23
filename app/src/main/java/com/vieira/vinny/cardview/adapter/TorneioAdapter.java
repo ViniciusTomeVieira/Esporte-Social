8e8a2c15a928e576fffdfe3f11c0ce5642f637c1
Eduardo Woloszyn <eduardowoloszyn78@gmail.com>
2018-11-23 19:46:34 -0200

    LIFE IS BEAUTIFUL
    I THINK LIFE IS BEATIFUL

--- app/src/main/java/com/vieira/vinny/cardview/adapter/TorneioAdapter.java
+++ app/src/main/java/com/vieira/vinny/cardview/adapter/TorneioAdapter.java
@@ -2,36 +2,62 @@ package com.vieira.vinny.cardview.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.vieira.vinny.cardview.R;
+import com.vieira.vinny.cardview.model.Torneio;
 
 import java.util.List;
 
 public class TorneioAdapter extends RecyclerView.Adapter<TorneioAdapter.MyViewAdapter> {
-
-    public TorneioAdapter() {
+    private List<Torneio> listaTorneio;
+    public TorneioAdapter(List<Torneio> l) {
+        listaTorneio = l;
     }
 
     @NonNull
     @Override
     public MyViewAdapter onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View itemLista = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_torneio,viewGroup,false);
+        return new TorneioAdapter.MyViewAdapter(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewAdapter myViewAdapter, int i) {
-
+        Torneio torneio = listaTorneio.get(i);
+        myViewAdapter.titulo.setText(torneio.getTitulo());
+        myViewAdapter.inscritos.setText(torneio.getInscritos());
+        myViewAdapter.cidade.setText(torneio.getCidade());
+        myViewAdapter.local.setText(torneio.getLocal());
+        myViewAdapter.taxa.setText(torneio.getTaxa());
+        myViewAdapter.imagemTorneio.setImageResource(torneio.getImagemTorneio());
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaTorneio.size();
     }
 
     public class MyViewAdapter extends RecyclerView.ViewHolder {
-
+        private TextView titulo;
+        private TextView cidade;
+        private TextView taxa;
+        private TextView local;
+        private TextView inscritos;
+        private ImageView imagemTorneio;
         public MyViewAdapter(@NonNull View itemView) {
             super(itemView);
+            titulo        = itemView.findViewById(R.id.tv_titulo);
+            cidade        = itemView.findViewById(R.id.tv_local);
+            taxa          = itemView.findViewById(R.id.tv_taxa);
+            local         = itemView.findViewById(R.id.tv_localizacao);
+            inscritos     = itemView.findViewById(R.id.tv_participantes);
+            imagemTorneio = itemView.findViewById(R.id.img_torneio);
         }
     }
 }
--- app/src/main/java/com/vieira/vinny/cardview/fragment/TorneioFragment.java
+++ app/src/main/java/com/vieira/vinny/cardview/fragment/TorneioFragment.java
@@ -3,17 +3,25 @@ package com.vieira.vinny.cardview.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.vieira.vinny.cardview.R;
+import com.vieira.vinny.cardview.adapter.TorneioAdapter;
+import com.vieira.vinny.cardview.model.Torneio;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TorneioFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private List<Torneio> listTorneio;
+    private Torneio torneio;
 
     public TorneioFragment() {
         // Required empty public constructor
@@ -23,8 +31,42 @@ public class TorneioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        initComponents(container);
+        loadAdapter();
+        loadManager(container);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_torneio, container, false);
     }
 
+    private void loadAdapter() {
+        TorneioAdapter adapter = new TorneioAdapter(listTorneio);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void loadManager(ViewGroup container) {
+        RecyclerView.LayoutManager manager =  new LinearLayoutManager(container.getContext());
+        recyclerView.setLayoutManager(manager);
+    }
+
+    private void initComponents(ViewGroup view) {
+        recyclerView = view.findViewById(R.id.recyclerView);
+    }
+
+    private void populaLista(){
+        torneio = new Torneio("Valendo uma caixa de Schin","São Valentim - RS",
+                "R$1.500,00","Bairro Terra a Vista", "10/16" , R.drawable.common_google_signin_btn_icon_dark);
+        listTorneio.add(torneio);
+        torneio = new Torneio("Valendo uma caixa de Brahma","Gaurama - RS",
+                "R$1.500,00","Bairro Tijolo a Vista", "10/16" , R.drawable.common_google_signin_btn_icon_dark);
+        listTorneio.add(torneio);
+        torneio = new Torneio("Valendo um Leitão","Ibirama - SC",
+                "R$1.500,00","Bairro Bila a Vista", "10/16" , R.drawable.common_google_signin_btn_icon_dark);
+        listTorneio.add(torneio);
+        torneio = new Torneio("Valendo uma Vaca","Campos Novos - SC",
+                "R$1.500,00","Bairro Borra a Vista", "10/16" , R.drawable.common_google_signin_btn_icon_dark);
+        listTorneio.add(torneio);
+        torneio = new Torneio("Valendo um cachorro","Imbituba - SC",
+                "R$1.500,00","Bairro Ban a Vista", "10/16" , R.drawable.common_google_signin_btn_icon_dark);
+        listTorneio.add(torneio);
+    }
 }
